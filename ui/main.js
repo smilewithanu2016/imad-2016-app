ba3c2e60a7742d68e173f84a799ba9d65609179d
smilewithanu2016 <smilewithanu2016@gmail.com>
2016-11-05 18:29:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,3 @@
 console.log('Loaded!');
 
-var submit=document.getElementById('submit');
-submit.onclick =function(){
 
-var request= new XMLHttpRequest();
-request.onreadystatechange= function () {
-if (request.readyState ===XMLHttpRequest.DONE){
-if (request.status === 200) {
-alert ("Logged in successfully");
-} else if( request.status === 403) {
-alert ("Username/password is incorrect");
-} else if( request.status === 500) {
-alert ("Something went wrong with the server");
-}
-}
-};
-
-var name = document.getElementById('userid').value;
-var password = document.getElementById('password').value;
-request.open('POST','http://smilewithanu2016.imad.hasura-app.io/ui/bloglogin.html',true);
-request.setRequestHeader('Content-Type','application/json');
-request.send(JSON.strinify({userid:userid, password:password}));
-};
