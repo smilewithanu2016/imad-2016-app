615551c0c8e2ac4569a9e571aef0d9a72433d3eb
smilewithanu2016 <smilewithanu2016@gmail.com>
2016-11-07 11:21:17 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 
