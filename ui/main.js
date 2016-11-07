566eee0dab3626f67ba4dd405720d8b9f77d7b14
smilewithanu2016 <smilewithanu2016@gmail.com>
2016-11-07 15:30:14 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,7 @@ function myFunction() {
    <label for="gender">Gender:</label>
   <input type="radio" name="gender" value="male">Male
   <input type="radio" name="gender" value="female">Female
-   <input type="submit" class="btn btn-primary" name="submit" formaction="main.js">Submit</button>
+   <input type="submit" name="submit">Submit</button>
 </div> 
        
 <script>
